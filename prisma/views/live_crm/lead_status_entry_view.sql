081f8f31d0a481781285becacdfa9b775feb415d
Your Name <you@example.com>
2026-04-29 12:52:41 +0000

    Revert "env updated"
    This reverts commit 769f98141018726edb756b9df4def2a405291001.

---
+++ prisma/views/live_crm/lead_status_entry_view.sql
@@ -0,0 +1,33 @@
+SELECT
+  `le`.`lead_entry_id` AS `lead_entry_id`,
+  `l`.`lead_id` AS `lead_id`,
+  `u2`.`name` AS `from_user_name`,
+  `u`.`name` AS `user_name`,
+  `le`.`from_rm_user_id` AS `from_rm_user_id`,
+  `le`.`rm_user_id` AS `rm_user_id`,
+  `ls1`.`lead_status_name` AS `from_status`,
+  `ls2`.`lead_status_name` AS `to_status`,
+  `u3`.`name` AS `created_by_name`,
+  `le`.`created_at` AS `created_at`,
+  `le`.`remarks` AS `remarks`,
+  date_format(`le`.`created_at`, '%d-%M-%Y %H:%i:%s') AS `display_created_at`
+FROM
+  (
+    (
+      (
+        (
+          (
+            (
+              `dev_crm`.`lead_status_entry` `le`
+              JOIN `dev_crm`.`lead` `l` ON((`le`.`lead_id` = `l`.`lead_id`))
+            )
+            JOIN `dev_crm`.`lead_status` `ls1` ON((`le`.`from_status_id` = `ls1`.`lead_status_id`))
+          )
+          JOIN `dev_crm`.`lead_status` `ls2` ON((`le`.`to_status_id` = `ls2`.`lead_status_id`))
+        )
+        JOIN `dev_crm`.`user` `u` ON((`le`.`rm_user_id` = `u`.`user_id`))
+      )
+      JOIN `dev_crm`.`user` `u2` ON((`le`.`from_rm_user_id` = `u2`.`user_id`))
+    )
+    JOIN `dev_crm`.`user` `u3` ON((`le`.`created_by` = `u3`.`user_id`))
+  )
---
+++ prisma/views/live_crm/leads_view.sql
@@ -0,0 +1,29 @@
+SELECT
+  `l`.`lead_id` AS `lead_id`,
+  `l`.`customer_name` AS `customer_name`,
+  `l`.`rm_user_id` AS `rm_user_id`,
+  `u`.`name` AS `assigned_to`,
+  `ss`.`sub_source_name` AS `sub_source_name`,
+  `l`.`mobile_no` AS `mobile_no`,
+  `p`.`project_name` AS `project_name`,
+  `ls`.`lead_status_name` AS `status`,
+  `l`.`lead_status_id` AS `status_id`,
+  `l`.`created_at` AS `created_at`,
+  `l`.`modified_at` AS `modified_at`,
+  `ls`.`label_color` AS `status_color`,
+  `l`.`schedule_date` AS `schedule_date`,
+  `l`.`flag` AS `flag`
+FROM
+  (
+    (
+      (
+        (
+          `live_crm`.`lead` `l`
+          JOIN `live_crm`.`lead_status` `ls` ON((`l`.`lead_status_id` = `ls`.`lead_status_id`))
+        )
+        JOIN `live_crm`.`project` `p` ON((`l`.`project_id` = `p`.`project_id`))
+      )
+      JOIN `live_crm`.`user` `u` ON((`l`.`rm_user_id` = `u`.`user_id`))
+    )
+    JOIN `live_crm`.`sub_source` `ss` ON((`l`.`sub_source_id` = `ss`.`sub_source_id`))
+  )
