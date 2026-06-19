de207217f6fd3dfcb6a1c28ba588fae177eab2da
Your Name <you@example.com>
2026-06-19 18:57:37 +0530

    mobile app

---
+++ src/app/api/app-mobile/dashboard/route.js
@@ -0,0 +1,247 @@
+import { no } from 'zod/v4/locales';
+import prisma from '../../../../../lib/prisma';
+import { getSessionFromToken } from "../../session";
+
+export async function GET(request) {
+  const token = getSessionFromToken();
+  try {
+    const where = { flag: 0 };
+    let lead_where = {
+      flag: 0,
+      lead_status_id: {
+        notIn: [6, 7]
+      }
+    };
+    if (token?.user_id && token?.role_id > 2) {
+      lead_where.rm_user_id = token.user_id;
+    }
+    // Calculate start and end of today
+    const startOfDay = new Date();
+    startOfDay.setHours(0, 0, 0, 0);
+    const endOfDay = new Date();
+    endOfDay.setHours(24, 0, 0, 0);
+
+    // Total leads
+    const totalLeads = await prisma.lead.count({ where: lead_where });
+
+    // Today's calls done (to_status_id = 3)
+    let calls_where = {
+      created_at: { gte: startOfDay, lt: endOfDay },
+      to_status_id: {not: 1},
+      flag: 0
+    }
+    if (token?.user_id && token?.role_id > 2) {
+      calls_where.rm_user_id = token.user_id;
+    }
+    const callsDoneTodays = await prisma.lead_status_entry.groupBy({
+      by: ['lead_id'],
+      where: calls_where,
+      _count: {
+        lead_id: true
+      }
+    });
+
+    const callsDoneToday = callsDoneTodays.length;
+
+    // Today's new leads (to_status_id = 1)
+    let total_leads_where = {
+      created_at: { gte: startOfDay, lt: endOfDay },
+      to_status_id: 1,
+      flag: 0
+    }
+    if (token?.user_id && token?.role_id > 2) {
+      total_leads_where.rm_user_id = token.user_id;
+    }
+    const totalLeadsTodays = await prisma.lead_status_entry.groupBy({
+      by: ['lead_id'],
+      where: total_leads_where,
+      _count: {
+        lead_id: true
+      }
+    });
+
+    const totalLeadsToday = totalLeadsTodays.length;
+
+    // Today's SVD (to_status_id = 5)
+
+    let total_svd_where = {
+      to_status_id: 5,
+      flag: 0
+    }
+    if (token?.user_id && token?.role_id > 2) {
+      total_svd_where.rm_user_id = token.user_id;
+    }
+    const svds = await prisma.lead_status_entry.groupBy({
+      by: ['lead_id'],
+      where: total_svd_where,
+      _count: {
+        lead_id: true
+      }
+    });
+    const svd = svds.length;
+
+    let total_closures_where = {
+      to_status_id: 7,
+      flag: 0
+    }
+    if (token?.user_id && token?.role_id > 2) {
+      total_closures_where.rm_user_id = token.user_id;
+    }
+    const closures = await prisma.lead_status_entry.groupBy({
+      by: ['lead_id'],
+      where: total_closures_where,
+      _count: {
+        lead_id: true
+      }
+    });
+    const NumberofClosuresDone = closures.length;
+
+    // Fetch all lead statuses
+    const lead_status = await prisma.lead_status.findMany({ where });
+    if (lead_status.length === 0) return new Response(JSON.stringify([]), { status: 200 });
+
+    // Fetch counts grouped by to_status_id
+    const counts = await prisma.lead_status_entry.groupBy({
+      by: ['to_status_id'],
+      _count: { to_status_id: true }
+    });
+    // console.log(counts);
+    // Map counts to lead_status
+    const lead_status_with_count = lead_status.map(status => {
+      const countObj = counts.find(c => c.to_status_id === status.lead_status_id);
+      return {
+        ...status,
+        leadcount: countObj?._count.to_status_id || 0
+      };
+    });
+
+    
+
+    const NumberofDaysLastBookingDone = 0;
+
+    const LastBookingDate = await prisma.lead.findFirst({
+      where: {
+        flag: 0,
+        lead_status_id: 7,
+        rm_user_id: token?.user_id && token?.role_id > 2 ? token.user_id : undefined
+      },
+      orderBy: {
+        closed_date: 'desc'
+      },
+      select: {
+        closed_date: true
+      }
+    });
+
+    const todayStart = new Date();
+    todayStart.setHours(0, 0, 0, 0);
+
+    const todayEnd = new Date();
+    todayEnd.setHours(23, 59, 59, 999);
+
+
+    // due and overdue counts
+    const dueCount = await prisma.lead.count({
+      where: {
+        flag: 0,
+        schedule_date: { gte: todayStart, lte: todayEnd }
+      }
+    });
+    const overdueCount = await prisma.lead.count({
+      where: {
+        flag: 0,
+        schedule_date: { lt: todayStart }
+      }
+    });
+    // console.log(lead_status_with_count);
+    let revenue_where = {
+      flag: 0,
+      lead_status_id: 7
+    }
+    if (token?.user_id && token?.role_id > 2) {
+      revenue_where.rm_user_id = token.user_id;
+    }
+    const revenueAmount = await prisma.lead.aggregate({
+      _sum: {
+        revenue: true
+      },
+      where: revenue_where
+    });
+    let revenue = 0;
+    if(revenueAmount._sum.revenue === null){
+      revenue = revenueAmount._sum.revenue = 0;
+    } else {
+      revenue = revenueAmount._sum.revenue = revenueAmount._sum.revenue?.toLocaleString('en-US', { style: 'currency', currency: 'INR' });
+    }
+
+    const ListData = [
+      {
+        id : "1",
+        coin : "Total Active Leads",
+        amount : totalLeads,
+        rate : "",
+        subTitle : 'In CRM',
+      },
+      {
+        id : "2",
+        coin : "Total Calls Done",
+        amount : callsDoneToday,
+        rate : "",
+        subTitle : 'Today',
+      },
+      {
+        id : "3",
+        coin : "Today Total Leads",
+        amount : totalLeadsToday,
+        rate : "",
+        subTitle : 'Today',
+      },
+      {
+        id : "4",
+        coin : "Svd",
+        amount : svd,
+        rate : "",
+        subTitle : 'Today',
+      },
+      {
+        id : "5",
+        coin : "Number of Closures",
+        amount : NumberofClosuresDone,
+        rate : "",
+        subTitle : 'Done  ',
+      },
+      {
+        id : "6",
+        coin : "Last Booking done",
+        amount : NumberofDaysLastBookingDone,
+        rate : "",
+        subTitle : 'Number of Days',
+      },
+      {
+        id : "7",
+        coin : "Revenue",
+        amount :  revenue,
+        rate : "",
+        subTitle : 'Total Generated ',
+      },
+    ]
+    return new Response(
+      JSON.stringify({ ListData}),
+      { status: 200, headers: { 'Content-Type': 'application/json' } }
+    );
+
+    // return new Response(
+    //   JSON.stringify({ totalLeads, callsDoneToday, totalLeadsToday, svd, lead_status: lead_status_with_count, dueCount, overdueCount, NumberofClosuresDone, LastBookingDate: LastBookingDate?.closed_date || null, TotalRevenue: revenueAmount._sum.revenue || 0 }),
+    //   { status: 200, headers: { 'Content-Type': 'application/json' } }
+    // );
+
+  } catch (error) {
+    console.error('Error fetching leads:', error);
+    return new Response(
+      JSON.stringify({ success: false, message: error.message }),
+      { status: 500, headers: { 'Content-Type': 'application/json' } }
+    );
+  }
+}
+
+export const dynamic = "force-dynamic";
---
+++ src/app/api/app-mobile/lead/route.js
@@ -0,0 +1,432 @@
+import { email } from 'zod';
+import prisma from '../../../../../lib/prisma';
+import { getSessionFromToken } from "../../session";
+
+/*
+INSERT INTO `lead_file` (`lead_file_id`, `lead_file_name`, `file_path`, `created_at`, `created_by`, `modified_at`, `modified_by`, `company_id`, `flag`) VALUES (NULL, 'Manual', '/', NULL, NULL, NULL, NULL, '0', '0');
+*/
+export async function GET(request) {
+  const token = getSessionFromToken();
+  const todayStart = new Date();
+  // todayStart.setHours(0, 0, 0, 0);
+
+  const todayEnd = new Date();
+  // todayEnd.setHours(23, 59, 59, 999);
+
+  const dashboardQry = null;
+  try {
+    const { searchParams } = new URL(request.url);
+
+    const id = parseInt(searchParams.get('id'));
+    let limit = parseInt(searchParams.get('limit')) || 100;
+    let page = parseInt(searchParams.get('page')) || 1;
+    let orderBy = searchParams.get('orderBy') || 'modified_at';
+    let view = searchParams.get('view');
+    let due_filter = searchParams.get('due_filter');
+    let assigned_to = searchParams.get('assigned_to');
+    let lead_status_id = searchParams.get('lead_status_id');
+    let dd_callsDoneToday = searchParams.get('dd_callsDoneToday');
+    let dd_totalLeadsToday = searchParams.get('dd_totalLeadsToday');
+    let dd_svd = searchParams.get('dd_svd');
+    let dd_closure = searchParams.get('dd_closure');
+    let search = searchParams.get('search') || "";
+    if (id) {
+      const dataItem = await prisma.leads_view.findFirst({
+        where: {
+          lead_id: id
+        }
+      });
+      return Response.json(dataItem);
+    }
+    if (view == "1") {
+      let vwhere = {
+        AND: []
+      };
+
+      vwhere.AND.push({ flag: 0 });
+      if (lead_status_id) {
+        vwhere.AND.push({ status_id: parseInt(lead_status_id) });
+      } else {
+        vwhere.AND.push({ status_id: { notIn: [6, 7] } });
+      }
+      if (search) {
+        vwhere.AND.push({
+          OR: [
+            { customer_name: { contains: search } },
+            { mobile_no: { contains: search } }
+          ]
+        });
+      }
+      if (due_filter === "1") {
+        todayStart.setHours(0, 0, 0, 0);
+        todayEnd.setHours(23, 59, 59, 999);
+        vwhere.AND.push({
+          OR: [
+            {
+              schedule_date: {
+                gte: todayStart,
+                lte: todayEnd,
+              },
+            },
+            {
+              AND: [
+                { schedule_date: null },
+                {
+                  created_at: {
+                    gte: todayStart,
+                    lte: todayEnd,
+                  },
+                },
+              ]
+            }
+          ]
+        });
+        vwhere.AND.push({ status_id: { notIn: [6, 7] } });
+      }
+
+      if (due_filter === "0") {
+        todayStart.setHours(0, 0, 0, 0);
+        vwhere.AND.push({
+          OR: [
+            {
+              schedule_date: {
+                lt: todayStart,
+              },
+            },
+            { schedule_date: null }
+          ]
+        });
+        vwhere.AND.push({ status_id: { notIn: [6, 7] } });
+      }
+
+      if (due_filter === "2") {
+        vwhere.AND.push({
+          schedule_date: {
+            gt: todayEnd,
+          }
+        });
+        vwhere.AND.push({ status_id: { notIn: [6, 7] } });
+      }
+      if (assigned_to) {
+        vwhere.rm_user_id = parseInt(assigned_to);
+      }
+      /** DATADASHBOARD QUERY********************** */
+      if (dd_callsDoneToday === "true") {
+        vwhere = {};
+        // Today's calls done (to_status_id = 3)
+        let calls_where = {
+          created_at: { gte: todayStart, lt: todayEnd },
+          to_status_id: { not: 1 },
+          flag: 0
+        }
+        if (token?.user_id && token?.role_id > 2) {
+          calls_where.rm_user_id = token.user_id;
+        }
+        const callsDoneTodays = await prisma.lead_status_entry.groupBy({
+          by: ['lead_id'],
+          where: calls_where,
+          _count: {
+            lead_id: true
+          }
+        });
+        const callsDoneToday = callsDoneTodays.map(item => item.lead_id);
+        vwhere.lead_id = { in: callsDoneToday };
+        // console.log(callsDoneTodays.length);
+      } if (dd_totalLeadsToday === "true") {
+        vwhere = {};
+        let total_leads_where = {
+          created_at: { gte: todayStart, lt: todayEnd },
+          to_status_id: 1,
+          flag: 0
+        }
+        if (token?.user_id && token?.role_id > 2) {
+          total_leads_where.rm_user_id = token.user_id;
+        }
+        const totalLeadsTodays = await prisma.lead_status_entry.groupBy({
+          by: ['lead_id'],
+          where: total_leads_where,
+          _count: {
+            lead_id: true
+          }
+        });
+
+        const totalLeadsToday = totalLeadsTodays.map(item => item.lead_id);
+        vwhere.lead_id = { in: totalLeadsToday };
+        // console.log(totalLeadsTodays.length);
+      } if (dd_svd === "true") {
+        vwhere = {};
+        let total_svd_where = {
+          to_status_id: 5,
+          flag: 0
+        }
+        if (token?.user_id && token?.role_id > 2) {
+          total_svd_where.rm_user_id = token.user_id;
+        }
+        const svds = await prisma.lead_status_entry.groupBy({
+          by: ['lead_id'],
+          where: total_svd_where,
+          _count: {
+            lead_id: true
+          }
+        });
+
+        const svdid = svds.map(item => item.lead_id);
+        vwhere.lead_id = { in: svdid };
+        // console.log(totalLeadsTodays.length);
+      } if (dd_closure === "true") {
+        vwhere = {};
+        let total_closures_where = {
+          to_status_id: 7,
+          flag: 0
+        }
+        if (token?.user_id && token?.role_id > 2) {
+          total_closures_where.rm_user_id = token.user_id;
+        }
+        const closures = await prisma.lead_status_entry.groupBy({
+          by: ['lead_id'],
+          where: total_closures_where,
+          _count: {
+            lead_id: true
+          }
+        });
+
+        const closuresid = closures.map(item => item.lead_id);
+        vwhere.lead_id = { in: closuresid };
+        // console.log(totalLeadsTodays.length);
+      } else if (lead_status_id) {
+        vwhere.status_id = parseInt(lead_status_id);
+      }
+      /** DATADASHBOARD END QUERY********************** */
+      if (token?.user_id && token?.role_id > 2 && !assigned_to) {
+        vwhere.rm_user_id = token.user_id;
+        const teamMembers = await prisma.user_team_member.findMany({
+          where: {
+            leader_id: token.user_id,
+            flag: 0
+          },
+          select: {
+            member_id: true
+          }
+        });
+        if (teamMembers && teamMembers.length > 0) {
+          const memberIds = teamMembers.map(member => member.member_id);
+          vwhere.rm_user_id = { in: [token.user_id, ...memberIds] };
+        }
+      }
+      // console.log(token.user_id);
+      console.log("vwhere:", JSON.stringify(vwhere));
+      const dataItems = await prisma.leads_view.findMany({
+        where: vwhere,
+        take: limit,
+        skip: (page - 1) * limit,
+        orderBy: { modified_at: 'asc' }
+      });
+      const totalCount = await prisma.leads_view.count({
+        where: vwhere
+      });
+      return Response.json({ data: dataItems, dashboard: dashboardQry, total: totalCount });
+    }
+
+    const dataItems = await prisma.lead.findMany({
+      where: {
+        flag: 0
+      },
+      take: limit,
+      orderBy: {
+        [orderBy]: 'desc'
+      }
+    });
+    const totalCount = await prisma.lead.count({
+      where: {
+        flag: 0
+      }
+    });
+    return Response.json({ data: dataItems, dashboard: dashboardQry, total: totalCount });
+  } catch (error) {
+    console.error('Error fetching leads:', error);
+    return Response.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
+
+
+export async function POST(request) {
+  const token = getSessionFromToken();
+  try {
+    const req = await request.json();
+    const duplicateLead = await prisma.lead.findFirst({
+      where: {
+        mobile_no: req.mobile_no,
+        // project_id: parseInt(req.project_id),
+        flag: 0
+      }
+    });
+    if (duplicateLead) {
+      return Response.json({ duplicateLead: true, success: false, message: "Lead with this mobile number already exists for the selected project." }, { status: 400 });
+    }
+    const newlead = await prisma.lead.create({
+      data: {
+        created_at: new Date(),
+        schedule_date: new Date(),
+        created_by: token?.user_id || null,
+        customer_name: req.customer_name,
+        mobile_no: req.mobile_no,
+        email_id: req.email_id ? req.email_id : null,
+        alternate_no: req.alternate_no ? req.alternate_no : null,
+        whatsapp_no: req.whatsapp_no ? req.whatsapp_no : null,
+        alternate_email: req.alternate_email ? req.alternate_email : null,
+        project_id: parseInt(req.project_id),
+        source_id: parseInt(req.source_id),
+        sub_source_id: parseInt(req.sub_source_id),
+        rm_user_id: parseInt(req.rm_user_id),
+        lead_status_id: parseInt(req.lead_status_id),
+        lead_file_id: parseInt(1),
+        remarks: req.remarks
+      }
+    });
+    if (newlead) {
+      await prisma.lead_status_entry.create({
+        data: {
+          lead_id: newlead?.lead_id,
+          created_by: token?.user_id ?? null,
+          created_at: new Date(),
+          from_status_id: 0,
+          to_status_id: 1,
+          from_rm_user_id: token?.user_id ?? null,
+          rm_user_id: token?.user_id ?? null,
+          remarks: "New Lead Created"
+        },
+      });
+    }
+    return Response.json({
+      success: true,
+      message: "Lead created successfully.",
+      lead: newlead
+    });
+  } catch (error) {
+    console.error('Error creating lead:', error);
+    return Response.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(request) {
+  const token = getSessionFromToken();
+  try {
+    const req = await request.json();
+    const id = req.slug;
+    const duplicateLead = await prisma.lead.findFirst({
+      where: {
+        mobile_no: req.mobile_no,
+        // project_id: parseInt(req.project_id),
+        lead_id: { not: id },
+        flag: 0
+      }
+    });
+    if (duplicateLead) {
+      return Response.json({ success: false, message: "Lead with this mobile number already exists for the selected project." }, { status: 400 });
+    }
+    const beforeLeadData = await prisma.lead.findUnique({
+      where: { lead_id: id }
+    });
+    console.log("Schedule Date:", new Date(req?.schedule_date));
+    const updatedlead = await prisma.lead.update({
+      where: { lead_id: id },
+      data: {
+        modified_at: new Date(),
+        customer_name: req.customer_name,
+        mobile_no: req.mobile_no,
+        email_id: req.email_id,
+        alternate_no: req.alternate_no,
+        whatsapp_no: req.whatsapp_no,
+        alternate_email: req.alternate_email,
+        project_id: parseInt(req.project_id),
+        source_id: parseInt(req.source_id),
+        sub_source_id: parseInt(req.sub_source_id),
+        rm_user_id: parseInt(req.rm_user_id),
+        lead_status_id: parseInt(req.lead_status_id),
+        remarks: req.remarks,
+        schedule_date: req?.schedule_date ? new Date(req?.schedule_date) : null, // use the parsed Date object
+        modified_by: token?.user_id || null,
+        closed_date: req?.closed_date ? new Date(req?.closed_date) : null, // use the parsed Date object
+        revenue: req?.revenue ? parseFloat(req?.revenue) : 0
+      }
+    });
+    if (updatedlead) {
+      await prisma.lead_status_entry.create({
+        data: {
+          lead_id: updatedlead?.lead_id,
+          created_by: token?.user_id ?? null,
+          created_at: new Date(),
+          from_status_id: beforeLeadData?.lead_status_id ?? 0,
+          to_status_id: updatedlead.lead_status_id,
+          from_rm_user_id: beforeLeadData?.rm_user_id ?? null,
+          rm_user_id: updatedlead?.rm_user_id ?? null,
+          remarks: req.status_remarks ? req.status_remarks : "Status Updated"
+        },
+      });
+    }
+    if (updatedlead && req.send_email) {
+      // console.log("Sending email notification for lead update...");
+      let html_body = `<h1>Lead Assigned</h1>
+      <p>The Following Lead Assigned to you, Please check and follow up.</p>
+
+      <!-- Table -->
+      <table class="content-table">
+        <tr>
+          <th>Lead ID</th>
+          <th>URL</th>
+        </tr>
+        <tr>
+          <td>` + updatedlead.lead_id + `</td>
+          <td><a href="` + process.env.NEXT_PUBLIC_BASE_URL + `/lead/` + updatedlead.lead_id + `/edit">View Lead</a></td>
+        </tr>
+      </table>
+
+      <p>Thank you for being part of our community!</p>`
+
+      try {
+        const mail_response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/send-email`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            email: process.env.NEXT_PUBLIC_ADMIN_EMAIL,
+            message: html_body,
+            subject: "New Lead Assigned - LID " + updatedlead.lead_id
+          })
+        });
+        // console.log("Email notification response:", mail_response);
+      } catch (emailError) {
+        console.error("Error sending email notification:", emailError);
+      }
+    }
+    return Response.json({
+      success: true,
+      message: "Lead updated successfully.",
+      lead: updatedlead
+    });
+  } catch (error) {
+    console.error('Error updating lead:', error);
+    return Response.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
+
+export async function DELETE(request) {
+  const token = getSessionFromToken();
+  try {
+    const { searchParams } = new URL(request.url);
+    const id = parseInt(searchParams.get('id'));
+    //console.log("Deleting lead with id:", id);
+    const deletedlead = await prisma.lead.update({
+      where: { lead_id: id },
+      data: { flag: 1, modified_by: token?.user_id || null, modified_at: new Date() }
+    });
+    return Response.json(deletedlead);
+  } catch (error) {
+    console.error('Error deleting lead:', error);
+    return Response.json({ success: false, message: error.message }, { status: 500 });
+  }
+}
+
+
+export const dynamic = "force-dynamic";
